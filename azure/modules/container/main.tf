cbf4916426d27f453505a0eb909d8b2cf86b3acc
dummyhurt <asxrra@gmail.com>
2023-03-14 19:51:28 +0000

    .............

--- azure/modules/container/main.tf
+++ azure/modules/container/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_container" "c" {
   name                  = var.c_name
-  storage_account_name  = var.sa_name
+  storage_account_name  = var.c_sa_name
   container_access_type = var.c_access_type
 }
